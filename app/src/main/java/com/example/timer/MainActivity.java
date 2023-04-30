096dd85d2d5de3422b50902632c1906dcbd7bf36
erees <ereese@luc.edu>
2023-04-30 12:35:32 -0500

    Added comments explaining methods/project!

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -68,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
         stopButton = findViewById(R.id.stop_button);
         incrementButton = findViewById(R.id.increment_button);
 
+
+        //Event listeners for each button. Start, Stop, Pause, and increment.
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -105,22 +107,32 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Reset timer method for abstraction. Just resets the time and increment count to 0. Used in StopTimer and onFinish
     private void resetTimer() {
         timeLeftInMilliseconds = 0;
         incrementCounter = 0;
         updateTimer();
     }
 
-
+    //Method used once start button is pressed.
     private void startTimer() {
+        //define a variable for an initial start time
         long initialTimeInMiliseconds;
+        //if the timeLeftInMiliseconds is 0, the initial time is set as the incrementCount in seconds once the button is pressed. This is a check
+        //on whether or not the timer is paused.
+        //the else statement is used for that pause mechanism in case timeLeft is not 0.
         if(timeLeftInMilliseconds == 0){
             initialTimeInMiliseconds = incrementCounter *1000;
         }else{
             initialTimeInMiliseconds = timeLeftInMilliseconds;
         }
-        //timeLeftInMilliseconds = incrementCounter * 1000;
+
+        //sets the increment counter to 0 once the timer starts so it does not
+        //add the previous increment value when trying to start again.
         incrementCounter = 0;
+        //Checks if the countDowntimer is null, and recreates/defines it.
+        //Provides onTick which decrements the time and updates the timer.
+        //Provides onFinish which resets the timer and plays audio once the time reaches 0.
         if (countDownTimer == null) {
             countDownTimer = new CountDownTimer(initialTimeInMiliseconds, 1000) {
                 @Override
@@ -144,6 +156,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Used in the stopbutton listener.
+    //if  the cuontdowntimer is not null, it will reset the timer completely.
     private void stopTimer() {
         resetTimer();
         if(countDownTimer != null) {
@@ -154,6 +168,9 @@ public class MainActivity extends AppCompatActivity {
         startButton.setText("Start");
     }
 
+    //Used in the pause button listener
+    // if the count down timer is not null, it stops the timer.
+    //It updates timeLeft, subtracting it by the incremented time in seconds so the timer resumes properly.
     private void pauseTimer() {
         if (countDownTimer != null) {
             countDownTimer.cancel();
@@ -164,6 +181,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Updates the timer display
     private void updateTimer() {
         int minutes = (int) timeLeftInMilliseconds / 60000;
         int seconds = (int) timeLeftInMilliseconds % 60000 / 1000;
@@ -172,6 +190,9 @@ public class MainActivity extends AppCompatActivity {
         timerTextView.setText(timeLeftFormatted);
     }
 
+
+
+    //used to play a default ringtone and is used in onFinish.
     private void playRingtone(Context context) {
         if (mediaPlay !=null) {
             mediaPlay.stop();
